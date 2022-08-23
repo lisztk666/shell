c381fea6f0994b9f8f92e9d9c77d95dfad31fd8a
liszt666 <75466918+lisztk666@users.noreply.github.com>
2022-08-23 10:32:40 +0800

    加入重啟 vm

--- psave.sh
+++ psave.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 #ping 主機是否存在, 0:正常 1:不正常
 #參數區
-PINGHOST="192.168.100.8"
-HOSTNAME="填入主機名稱"
+PINGHOST="vm_ip"
+HOSTNAME="客戶名"
 NOWTIME=$(date +'%Y%m%d-%H:%M:%S')
 GOODMSG=("$HOSTNAME主機_$PINGHOST-於$NOWTIME-恢復正常")
 ERRORMSG=("$HOSTNAME主機_$PINGHOST-於$NOWTIME-發生故障")
 TERRORMSG="/etc/sh/terrormsg.sh"
+VM=("pve_編號")
+QM_ON=("/usr/sbin/qm start $VM")
+QM_TIME=0
 DAY=1
 HOURS=8h
-SEC=5s
+SEC=4s
 
-#正常0 ,不正常1
+#正常0 不正常1
 KEYOK=0
 
-#虛擬機開機準備,休息300s
-sleep 300s
+#虛擬機開機準備
+#sleep 300s
+sleep 3s
 echo -e "預設 KEYOK=$KEYOK 正常\n"
 while true
 do
@@ -25,15 +29,38 @@ do
   while true
   do
    echo "login 2 while"
+   echo "斷線,進入第2次迴圈"
    sleep 3s
    NOWTIME=$(date +'%Y%m%d-%H:%M:%S')
    GOODMSG=("$HOSTNAME主機_$PINGHOST-於$NOWTIME-恢復正常")
-   #ping $PINGHOST -c 1 >>/dev/null && echo "2 ok" || $TERRORMSG $GOODMSG
+
+   echo "qm check"
+   echo "qm 比對開始"
+   QM_STATUS=$(qm list|grep $VM|awk -F" " '{print $3}')
+   echo $QM_STATUS
+   if [ $KEYOK == 1 ] && [ $QM_STATUS == "stopped" ] && [ $QM_TIME == 0 ]
+   then
+	 $TERRORMSG "$VM_start"
+    echo "qm call start"
+    echo $QM_ON
+    #啟動次數+1
+    $[QM_TIME++] 
+    $QM_ON
+    sleep 90s
+   else
+	 $TERRORMSG "$VM_跳過"
+   echo "qm continue"
+   QM_TIME=0
+    continue
+   fi
+
    ping $PINGHOST -c 1 >>/dev/null && KEYOK=0 || KEYOK=1
+
    if [ $KEYOK == 0 ]
     then
      echo "logout 2 "
-     $TERRORMSG $GOODMSG
+     echo "確認上線脫離2次迴圈 "
+	 $TERRORMSG $GOODMSG
      break
     fi
   done
