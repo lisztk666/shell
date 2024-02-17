6b01089b3fcf61c398fee53b5a4fe5252825589f
liszt666 <75466918+lisztk666@users.noreply.github.com>
2024-02-17 11:32:08 +0800

    Update pve_update8.sh
    add btop

--- pve_update8.sh
+++ pve_update8.sh
@@ -25,7 +25,7 @@ apt update -y && apt upgrade -y && pveam update
 
 nice -n -20 apt install htop iftop tree vim lshw lm-sensors screen iotop  nfs-kernel-server autofs cifs-utils wakeonlan smartmontools  p7zip-full zfs-zed net-tools -y
 nice -n -20 apt install dos2unix vsftpd mutt samba ncdu apcupsd sysstat  multipath-tools lsscsi  ifstat iptraf-ng  nethogs  bmon cbm nload terminator tmux cpufrequtils -y
-nice -n -20 apt install  ntpdate pv python3-pip s-tui gpart ethtool git  hwloc neofetch bridge-utils  -y
+nice -n -20 apt install  ntpdate pv python3-pip s-tui gpart ethtool git  hwloc neofetch bridge-utils  btop -y
 nice -n -20 apt install glances -y
 
 #hddtemp 
