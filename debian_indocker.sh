2436da442a00026e3335200bb62ad840449b95a6
liszt666 <75466918+lisztk666@users.noreply.github.com>
2024-02-17 11:59:34 +0800

    Update debian_indocker.sh
    apt install tools

--- debian_indocker.sh
+++ debian_indocker.sh
@@ -16,3 +16,10 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+#apt install tools
+apt install samba mutt dos2unix vsftpd tmux s-tui nload lsscsi ntpdate mc cbm bmon mdadm htop glances btop net-tools -y
+apt install iftop iotop gpart ethtool git hwloc tree screen vim cifs-utils wakeonlan smartmontools p7zip-full ifstat apcupsd -y
+
+mkdir /etc/sh /backup /sysvol/docker -p
+
