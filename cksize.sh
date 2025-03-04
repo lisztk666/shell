4146269603ac6b33301ff840a7975167240682fe
liszt666 <75466918+lisztk666@users.noreply.github.com>
2025-03-04 18:36:06 +0800

    Update cksize.sh

--- cksize.sh
+++ cksize.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+TERRORMSG="/etc/sh/terrormsg.sh"
+HOSTNAME=$(hostname)
 df -Th | tail -n +2 | awk '{ print $6 " " $1 }' | while read output;
 do
   # 取出使用量（百分比）
@@ -10,6 +12,7 @@ do
   # 若用量大於 90% 則用 Email 發出警告訊息
   if [ $usep -ge 95 ]; then
     echo "Running out of space \"$partition ($usep%)\" on $(hostname) as on $(date)" |
-      mail -s "Alert: Almost out of disk space $usep%" you@somewhere.com
+      mail -s "Alert: Almost out of disk space $usep%" $HOSTNAME@somewhere.com
+      $TERRORMSG $HOSTNAME "Alert_Almost_most_out_of_disk_space_$usep%"
   fi
 done
