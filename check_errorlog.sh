2302953227c73b082ecafdad4c75a052213516f6
liszt666 <75466918+lisztk666@users.noreply.github.com>
2025-03-10 15:27:08 +0800

    Update check_errorlog.sh

--- check_errorlog.sh
+++ check_errorlog.sh
@@ -1,24 +1,22 @@
 #!/bin/bash
 PATH="$PATH"
 $HOSTNAME=$(hostname)
-
+SERVERN="客戶名_IP"
+CMSG="客戶名_week_`date +%u`"
+EMSG="EnglishName_Backup_week_`date +%u`"
+AMSG="$HOSTNAME_Backup_week_`date +%u`"
 LOG="/backup/log/$HOSTNAME_`date +%Y%m%d`.log"
 
 DIRLOG="/backup/mis/log/"
-HGLOG="/backup/mis/log/$HOSTNAME_`date +%Y%m%d`_hs_good.log"
-HBLOG="/backup/mis/log/$HOSTNAME_`date +%Y%m%d`_hs_error.log"
-SGLOG="/backup/mis/log/$HOSTNAME_`date +%Y%m%d`_share_good.log"
-SBLOG="/backup/mis/log/$HOSTNAME_`date +%Y%m%d`_share_error.log"
+HGLOG="/backup/mis/log/$HOSTNAME-`date +%Y%m%d`_hs_good.log"
+HBLOG="/backup/mis/log/$HOSTNAME-`date +%Y%m%d`_hs_error.log"
+SGLOG="/backup/mis/log/$HOSTNAME-`date +%Y%m%d`_share_good.log"
+SBLOG="/backup/mis/log/$HOSTNAME-`date +%Y%m%d`_share_error.log"
 
 LOGDAY="`date +%Y%m%d`"
 NOWTIME="date"
 DATEFMT="%Y/%m/%d-%R"
 DATFEMT_WEEK="week_`date +%u`"
-
-SERVERN="客戶名_IP"
-CMSG="客戶名_week_`date +%u`"
-EMSG="EnglishName_Backup_week_`date +%u`"
-AMSG="$HOSTNAME_Backup_week_`date +%u`"
 
 SOURCE="/sysvol"
 SOURCEHS="/sysvol/hs"
