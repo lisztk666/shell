c2a346261584ee1d3a1de9a4dcd47be19d2beebc
liszt666 <75466918+lisztk666@users.noreply.github.com>
2025-03-10 14:41:31 +0800

    Update mmount.sh
    add ro

--- mmount.sh
+++ mmount.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 /usr/bin/mount --bind /backup/scan /home/scan
 /usr/bin/mount --bind /backup/fax /home/ffax
+#/usr/bin/mount -t cifs -o ro,username=xxxx,password=xxxx,uid=1000,gid=1000,dir_mode=0777,file_mode=0775 "//server/" /mnt/server/
 
