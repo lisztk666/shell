5b5b81d5cb25213e6c996da97ae35178bb99f089
liszt666 <75466918+lisztk666@users.noreply.github.com>
2025-03-22 14:26:05 +0800

    Update pve_update8.sh

--- pve_update8.sh
+++ pve_update8.sh
@@ -25,7 +25,8 @@ apt update -y && apt upgrade -y && pveam update
 nice -n -20 apt install htop iftop tree vim lshw lm-sensors screen iotop  nfs-kernel-server autofs cifs-utils wakeonlan smartmontools  p7zip-full zfs-zed net-tools -y
 nice -n -20 apt install dos2unix vsftpd mutt samba ncdu apcupsd sysstat  multipath-tools lsscsi  ifstat iptraf-ng  nethogs  bmon cbm nload terminator tmux cpufrequtils -y
 nice -n -20 apt install  ntpdate pv s-tui gpart ethtool git  hwloc bridge-utils  btop lnav ntfs-3g progress gddrescue mdadm -y
-nice -n -20 apt install glances -y
+nice -n -20 apt install glances rsyslog -y
+systemctl enable rsyslog --now
 
 #github dpkg download
 wget https://github.com/fastfetch-cli/fastfetch/releases/download/2.12.0/fastfetch-linux-amd64.deb
