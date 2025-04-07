02d78cc5158cf251f820770b729ae95dda7b7ba2
liszt666 <75466918+lisztk666@users.noreply.github.com>
2025-04-08 02:29:04 +0800

    Update check_errorlog.sh

--- check_errorlog.sh
+++ check_errorlog.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 PATH="$PATH"
-HOSTNAME="$(hostname)"
-SERVERN="客戶名_IP"
+#HOSTNAME="$(hostname)"
+#SERVERN="客戶名_IP"
+HOSTNAME=$1
+SERVERN=$2
+
 CMSG="客戶名_week_`date +%u`"
 EMSG="EnglishName_Backup_week_`date +%u`"
 AMSG="$HOSTNAME_Backup_week_`date +%u`"
 LOG="/backup/log/$HOSTNAME_`date +%Y%m%d`.log"
 
 DIRLOG="/backup/mis/log/"
-HGLOG="/backup/mis/log/$HOSTNAME-`date +%Y%m%d`_hs_good.log"
-HBLOG="/backup/mis/log/$HOSTNAME-`date +%Y%m%d`_hs_error.log"
-SGLOG="/backup/mis/log/$HOSTNAME-`date +%Y%m%d`_share_good.log"
-SBLOG="/backup/mis/log/$HOSTNAME-`date +%Y%m%d`_share_error.log"
+#HGLOG="/backup/mis/log/$HOSTNAME-`date +%Y%m%d`_hs_good.log"
+#HBLOG="/backup/mis/log/$HOSTNAME-`date +%Y%m%d`_hs_error.log"
+#SGLOG="/backup/mis/log/$HOSTNAME-`date +%Y%m%d`_share_good.log"
+#SBLOG="/backup/mis/log/$HOSTNAME-`date +%Y%m%d`_share_error.log"
+HBLOG=$3
 
 LOGDAY="`date +%Y%m%d`"
 NOWTIME="date"
@@ -28,4 +32,4 @@ SENDMSG="/etc/sh/tsendmsg.sh"
 SENDERROR="/etc/sh/terrormsg.sh"
 
 cat $HBLOG|grep -v "=" && $SENDERROR $SERVERN $HBLOG
-cat $SBLOG|grep -v "=" && $SENDERROR $SERVERN $SBLOG
+#cat $SBLOG|grep -v "=" && $SENDERROR $SERVERN $SBLOG
