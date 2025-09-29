73af0e6b3fddc98eb5bf5890d769679f9330ec9a
liszt666 <75466918+lisztk666@users.noreply.github.com>
2025-09-29 21:33:54 +0800

    Update terrormsg.sh
    fix $1 $2

--- terrormsg.sh
+++ terrormsg.sh
@@ -7,15 +7,15 @@ fi
 
 TOKEN="MYTOKEN"
 CHAT_ID="MYID"
-Message=$1
-FilePath=$2
+Message="$1"
+FilePath="$2"
 
 CMD1="https://api.telegram.org/bot$TOKEN/sendMessage"
 CMD2="https://api.telegram.org/bot$TOKEN/sendDocument"
 
-curl -s -X POST $CMD1 -d chat_id=$CHAT_ID -d text="$Message" >/dev/null 2>&1
-#curl -s -X POST $CMD1 -d chat_id=$CHAT_ID -d text="$Message"
+curl -s -X POST "$CMD1" -d chat_id="$CHAT_ID" -d text="$Message" >/dev/null 2>&1
+#curl -s -X POST "$CMD1" -d chat_id="$CHAT_ID" -d text="$Message"
 
 if [ -n "$FilePath" ]; then
-  curl $CMD2 -F chat_id=$CHAT_ID -F document=@"$FilePath" >/dev/null 2>&1
+  curl "$CMD2" -F chat_id="$CHAT_ID" -F document=@"$FilePath" >/dev/null 2>&1
 fi
