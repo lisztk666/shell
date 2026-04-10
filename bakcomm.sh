45e8ab8c28e2476bba0447b76e63bc7771156965
liszt666 <75466918+lisztk666@users.noreply.github.com>
2026-04-10 13:47:15 +0800

    Update bakcomm.sh

--- bakcomm.sh
+++ bakcomm.sh
@@ -1,7 +1,61 @@
-#!//bin/bash
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+# ===== 嚴格模式（一定要）=====
+set -o errexit
+set -o pipefail
+set -o nounset
+
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "$SCRIPT_DIR/common.sh"
-trap 'if [[ $? -ne 0 ]]; then echo "$(date) [SCRIPT FAIL]" | tee -a "$FAIL_LOG"; fi' EXIT
+#cd /etc/sh
+source "/etc/sh/common.sh"
+
+# ===== 錯誤處理 =====
+error_handler() {
+    local exit_code=$?
+    local line_no=$1
+    local cmd="$BASH_COMMAND"
+    local mail="liszt@ui.idv.tw"
+
+
+    local host
+    host="$(hostname)_至信"
+
+    local subject="[FAIL] Backup Script Error on $host"
+
+    local body
+
+    terrormsg "$subject"
+    body=$(cat <<EOF
+
+    "
+🚨 Backup Script 發生錯誤
+
+主機: $host
+時間: $(date)
+錯誤碼: $exit_code
+錯誤行數: $line_no
+錯誤指令: $cmd
+
+===== 最近 FAIL LOG =====
+$(tail -n 50 "$FAIL_LOG" 2>/dev/null)
+
+===== 最近 SUCCESS LOG =====
+$(tail -n 20 "$SUCCESS_LOG" 2>/dev/null)
+
+EOF
+)
+
+    # 📩 寄信
+    echo "$body" | mail -s "$subject" "$mail"
+    terrormsg "$body"
+
+    # 寫入 log
+    echo "$(date) [SCRIPT FAIL] line:$line_no code:$exit_code cmd:$cmd" | tee -a "$FAIL_LOG"
+}
+
+# ===== 綁定 trap =====
+trap 'error_handler $LINENO' ERR
+
 
 #開始執行
 print_system_header
