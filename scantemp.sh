08db7ef0800dc0d4135a8592a7ae0d6c3cd8abc6
liszt666 <75466918+lisztk666@users.noreply.github.com>
2026-04-24 17:00:08 +0800

    Update scantemp.sh

--- scantemp.sh
+++ scantemp.sh
@@ -1,25 +1,27 @@
 #!/bin/bash 
-#1110513 偵測溫度,sensors 需自行調整
+#1150424 偵測溫度,sensors 需自行調整
 CNAME="客戶名"
-SENDMSG="/etc/sh/tsendmsg.sh"
-CPUTEMP=$(sensors|grep Package|awk -F" " '{print $4}'|sed s/^\+//g|awk -F"." '{print $1}')
-GPUTEMP=$(sensors|grep GPU -n1|grep temp|awk -F" " '{print $2}'|sed  s/^\+//g|awk -F"." '{print $1 }')
+SENDMSG="/etc/sh/terrormsg.sh"
+
+CPUTEMP="$(sensors | grep -E 'Core 0|Package id 0|Tctl|Tdie'|sed s/+//|awk -F"." '{print $1}'|awk -F" " '{print $2}')"
+#GPUTEMP="$(sensors|grep GPU -n1|grep temp|awk -F" " '{print $2}'|sed  s/^\+//g|awk -F"." '{print $1 }')"
 
 #上限
-CPUMAX="75"
+CPUMAX="95"
 GPUMAX="80"
 
-
-if [ $CPUTEMP -ge $CPUMAX ] || [ $GPUTEMP -ge $GPUMAX  ]
+#if [ $CPUTEMP -ge $CPUMAX ] || [ $GPUTEMP -ge $GPUMAX  ]
+if [ "$CPUTEMP" -ge "$CPUMAX" ] 
 then
     echo "temp too hot"
     ERRORMSG="$CNAME :=
-    CPU:=現在溫度:$CPUTEMP  上限溫度:$CPUMAX
-    GPU:=現在溫度:$GPUTEMP  上限溫度:$GPUMAX
+    "$(date '+%Y-%m-%d %H:%M:%S')" CPU:=現在溫度:"$CPUTEMP"  上限溫度:"$CPUMAX"
 
-    $SENDMSG "$ERRORMSG"
+#    "$(date '+%Y-%m-%d %H:%M:%S')" GPU:=現在溫度:"$GPUTEMP"  上限溫度:"$GPUMAX"
+
+    "$SENDMSG" "$ERRORMSG"
 else
-    echo "coretemp is normal"
+    echo $(date '+%Y-%m-%d %H:%M:%S') - coretemp is normal"
  fi
 
 #
