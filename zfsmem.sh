79b816440863272c942327d8c0d25a786d8811ca
liszt666 <75466918+lisztk666@users.noreply.github.com>
2026-09-19 09:42:39 +0800

    Update zfsmem.sh
    mix to min

--- zfsmem.sh
+++ zfsmem.sh
@@ -9,7 +9,7 @@ MEM_MIN=$(($MEM_MAX - 1))
 echo "MEM_MIN"=$MEM_MIN
  
 echo "options zfs zfs_arc_max=$MEM_MAX">/etc/modprobe.d/zfs.conf
-echo "options zfs zfs_arc_mix=$MEM_MIN">>/etc/modprobe.d/zfs.conf
+echo "options zfs zfs_arc_min=$MEM_MIN">>/etc/modprobe.d/zfs.conf
 echo "$MEM_MAX" >/sys/module/zfs/parameters/zfs_arc_max
 echo "$MEM_MIN" >/sys/module/zfs/parameters/zfs_arc_min
  
